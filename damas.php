d599489f4e84f542a700441767ee7fb279dd8eba
angelone191 <angelone191@gmail.com>
2017-09-27 13:13:47 +0800

    versi 1

---
+++ damas.php
@@ -0,0 +1,11 @@
+<?php
+	echo "ni komang karina wardani";
+
+	for($i=1;$i<=100;$i++) {
+		echo $i ."<br>";
+	}
+?>
+
+<a href="index.php">link</a>
+
+	
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	echo "damas ganteng <br>";
+
+	for($a=1;$a<=20;$a++){
+		echo $a."<br>";
+	}
+?>
+<a href="damas.php">link ke damas here</a>
